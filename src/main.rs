c28e4365b788c3478a7d4aa2bac8aaeffc581d1d
LevKruglyak <lev.kruglyak2014@gmail.com>
2022-06-06 20:26:31 -0700

    add egui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use eframe::egui;
+
 fn main() {
-    println!("Hello, world!");
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("Ray Tracing Demo", native_options,
+        Box::new(|cc| Box::new(RayTracingApp::new(cc))));
+}
+
+#[derive(Default)]
+struct RayTracingApp {
+}
+
+impl RayTracingApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::default()
+    }
+}
+
+impl eframe::App for RayTracingApp {
+   fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+       egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+       });
+   }
 }
