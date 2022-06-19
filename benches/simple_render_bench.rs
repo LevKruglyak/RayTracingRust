024cdabdcb2991b3e38091516af841e2f2a5ef61
LevKruglyak <lev.kruglyak2014@gmail.com>
2022-06-19 05:36:22 -0700

    modify benchmark

--- benches/simple_render_bench.rs
+++ benches/simple_render_bench.rs
@@ -1,12 +1,24 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use ray_tracing_rust::render::RayTracingDemo;
+use ray_tracing_rust::{scene::Scene, material::Dielectric, objects::Sphere, utils::types::{Float, Vec3}, render::RayTracingDemo};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let contents =
-        std::fs::read_to_string("scenes/benchmark.json").expect("error: could not read file!");
-    let scene = serde_json::from_str(&contents).unwrap();
-    let mut app = RayTracingDemo::new(100, 100, scene);
+    let mut scene = Scene::default();
+    scene.settings.enable_bvh_tree = true;
 
+    let default_material = scene.add_material(Box::new(Dielectric::new(1.5)));
+
+    // Add a bunch of objects
+    for x in -5..5 {
+        for y in -5..5 {
+                scene.add_object(Box::new(Sphere::new(
+                        Vec3::new(x as Float, y as Float, 1.0),
+                        0.5,
+                        default_material,
+                        )));
+        }
+    }
+
+    let mut app = RayTracingDemo::new(100, 100, scene);
     c.bench_function("render", |b| b.iter(|| app.update()));
 }
 
